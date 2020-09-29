60a66608929611b94a4faf982b9c1e392d6e4edb
diaprilia <sucisondakh00@gmail.com>
2020-09-29 20:32:14 +0700

    fourth commit

---
+++ pertemuan_11/index.js
@@ -0,0 +1,13 @@
+//Exercise
+//convert to ES6 Syntax
+const yearUntilRetirement = (year,firstName) => {
+    var age=28;
+    var retirement=65-age;
+    if(retirement > 0){
+      console.log(`${firstName} retired in ${retirement} years. `);
+    }
+    else{
+      console.log(`${firstName} has already retired`);
+    }
+  }
+  yearUntilRetirement(2035,'andi');
