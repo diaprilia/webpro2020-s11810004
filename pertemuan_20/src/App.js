de3c50e174fc692003d1ad24d152fe7c15a8ee52
diaprilia <sucisondakh00@gmail.com>
2020-11-12 20:00:01 +0700

    pertemuan_23

--- pertemuan_20/src/App.js
+++ pertemuan_20/src/App.js
@@ -1,24 +1,82 @@
+// import './App.css';
+// import Card from './Components/Card';
+// import {person} from './Utils/person';
+// import Counter from "./Components/Counter" 
+
+// function App() {
+//   return (
+//     <>
+//       {/*person.map((item) =>{
+//         return(
+//           <Card
+//             name={item.name}
+//             institution={item.institution}
+//             address={item.address}
+//             phone={item.phone}
+//           />
+//         );
+//       })*/}
+//       <Counter title = "Counter Component"/>
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+//React People
+import React from 'react';
 import './App.css';
-import Card from './Components/card';
-import {person} from './Utils/person';
-import Counter from "./Components/counter" 
-
-function App() {
-  return (
-    <>
-      {/*person.map((item) =>{
-        return(
-          <Card
-            name={item.name}
-            institution={item.institution}
-            address={item.address}
-            phone={item.phone}
-          />
-        );
-      })*/}
-      <Counter title = "Counter Component"/>
-    </>
-  );
+const sortByPower = (a, b) => {
+    return b.power - a.power;
 }
+const List = props => {
+return (
+<>
+<h1>React People!</h1>
 
+<ul className="list">
+{props.data.sort(sortByPower).map((person) => (
+<li key={person.name} className="list-item">
+<div className="list-item-image-container">
+<img src={person.thumbnail} />
+</div>
+<span className="list-item-name">
+{person.name} ( Power:{' '}
+<span className="list-item-power">{person.power}</span> )
+</span>
+</li>
+))}
+</ul>
+</>
+);
+}
+const App = () => {
+const human = [
+{
+name: 'Kuririn',
+thumbnail:
+        'https://upload.wikimedia.org/wikipedia/pt/6/63/Kuririn_42311.png',
+power: 10
+},
+{
+name: 'Bulma',
+thumbnail: 
+        'https://upload.wikimedia.org/wikipedia/hu/1/1c/Bulma.png',
+power: 3
+},
+{
+name: 'Chi-chi',
+thumbnail:
+        'https://upload.wikimedia.org/wikipedia/hu/8/84/Chi-chi_dragon_ball_anime.ppg',
+
+power: 30
+}
+];
+return (
+<div className="App">
+<List data={human} />
+</div>
+);
+}
 export default App;
