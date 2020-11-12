5bd8d5d11a1f3cb518c1a0e2748c9d95bfad0a98
diaprilia <sucisondakh00@gmail.com>
2020-11-12 09:05:23 +0700

    pertemuan_22

--- pertemuan_20/src/App.js
+++ pertemuan_20/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Card from './Components/card';
 import {person} from './Utils/person';
- 
+import Counter from "./Components/counter" 
 
 function App() {
   return (
     <>
-      {person.map((item) =>{
+      {/*person.map((item) =>{
         return(
           <Card
             name={item.name}
@@ -15,9 +15,10 @@ function App() {
             phone={item.phone}
           />
         );
-      })}
+      })*/}
+      <Counter title = "Counter Component"/>
     </>
   );
-};
+}
 
 export default App;
