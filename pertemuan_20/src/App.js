ed9d836409dde703bd464937c73f4d33aa0adc79
diaprilia <sucisondakh00@gmail.com>
2020-11-12 20:04:43 +0700

    pertemuan_23_react_people

--- pertemuan_20/src/App.js
+++ pertemuan_20/src/App.js
@@ -1,7 +1,7 @@
 // import './App.css';
-// import Card from './Components/Card';
+// import Card from './Components/card';
 // import {person} from './Utils/person';
-// import Counter from "./Components/Counter" 
+// import Counter from "./Components/counter" 
 
 // function App() {
 //   return (
@@ -23,60 +23,52 @@
 
 // export default App;
 
-
-//React People
 import React from 'react';
 import './App.css';
 const sortByPower = (a, b) => {
-    return b.power - a.power;
+  return b.power - a.power;
 }
 const List = props => {
-return (
-<>
-<h1>React People!</h1>
-
-<ul className="list">
-{props.data.sort(sortByPower).map((person) => (
-<li key={person.name} className="list-item">
-<div className="list-item-image-container">
-<img src={person.thumbnail} />
-</div>
-<span className="list-item-name">
-{person.name} ( Power:{' '}
-<span className="list-item-power">{person.power}</span> )
-</span>
-</li>
-))}
-</ul>
-</>
-);
+  return (<>
+    <h1>React People!</h1>
+      <ul className="list">
+        {props.data.sort(sortByPower).map((person) => (
+          <li key={person.name} className="list-item">
+            <div className="list-item-image-container">
+              <img src={person.thumbnail} />
+            </div>
+            <span className="list-item-name">
+              {person.name} ( Power:{' '}
+            <span className="list-item-power">{person.power}</span> )
+          </span>
+        </li>
+      ))}
+    </ul>
+  </>
+  );
 }
 const App = () => {
-const human = [
-{
-name: 'Nami',
-thumbnail:
-        'https://upload.wikimedia.org/wikipedia/pt/https://www.deviantart.com/onepieceworldproject/art/One-Piece-Nami-5291880746/63/Kuririn_42311.png',
-power: 10
-},
-{
-name: 'Bulma',
-thumbnail: 
-        'https://upload.wikimedia.org/wikipedia/hu/1/1c/Bulma.png',
-power: 3
-},
-{
-name: 'Chi-chi',
-thumbnail:
-        'https://upload.wikimedia.org/wikipedia/hu/8/84/Chi-chi_dragon_ball_anime.ppg',
-
-power: 30
-}
-];
-return (
-<div className="App">
-<List data={human} />
-</div>
-);
+  const human = [
+    {
+      name: 'Krillin',
+      thumbnail:'https://upload.wikimedia.org/wikipedia/pt/6/63/Kuririn_42311.png',
+      power: 10
+    },  
+    {
+      name: 'Bulma',
+      thumbnail: 'https://upload.wikimedia.org/wikipedia/hu/1/1c/Bulma.png',
+      power: 3
+    },
+    {
+      name: 'Chi-chi',
+      thumbnail:'https://upload.wikimedia.org/wikipedia/hu/8/84/Chi-chi_dragon_ball_anime.jpg',
+      power: 30
+    }
+  ];
+  return (
+    <div className="App">
+      <List data={human} />
+    </div>
+  );
 }
 export default App;
