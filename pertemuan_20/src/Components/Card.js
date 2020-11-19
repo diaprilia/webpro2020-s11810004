ac421e9a5f7881e734c2a758d5de4f2eaaf37968
diaprilia <sucisondakh00@gmail.com>
2020-11-19 10:43:20 +0700

    pertemuan_24

--- pertemuan_20/src/Components/Card.js
+++ pertemuan_20/src/Components/Card.js
@@ -1,27 +1,12 @@
-// import react from 'react';
-
-// const Card = ({name, institution, address, phone}) =>{
-//     return (
-//         <div className="card-container">
-//             <p>{name}</p>
-//             <p>{institution}</p>
-//             <p>{address}</p>
-//             <p>{phone}</p>
-//         </div>
-//     )
-// }
-
-// export default Card;
-
   
-import React from 'react';
+import react from 'react'
 
-const Card = ({name, username, email, phone}) =>{
+const Card = ({name, institution, address, phone}) =>{
     return (
         <div className="card-container">
             <p>{name}</p>
-            <p>{username}</p>
-            <p>{email}</p>
+            <p>{institution}</p>
+            <p>{address}</p>
             <p>{phone}</p>
         </div>
     )
